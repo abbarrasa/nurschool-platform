17f896917e5b9077e48d3ffbbc3f66319e90c9dc
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2020-08-15 22:25:49 +0200

    Login and Registration pages

---
+++ src/Form/Type/RegistrationFormType.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the Nurschool project.
+ *
+ * (c) Nurschool <https://github.com/abbarrasa/nurschool>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nurschool\Form\Type;
+
+use Nurschool\Validator\Constraints\Password;
+use FOS\UserBundle\Form\Type\RegistrationFormType as FOSUserRegistrationFormType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
+
+class RegistrationFormType extends FOSUserRegistrationFormType
+{
+    /** @var UrlGeneratorInterface */
+    private $router;
+
+    public function __construct(UrlGeneratorInterface $router, $class)
+    {
+        parent::__construct($class);
+        $this->router = $router;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => false,
+                'attr' => ['placeholder' => 'form.email'],
+                'translation_domain' => 'FOSUserBundle'
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'options' => [
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'first_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'form.password'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'form.password_confirmation'
+                    ]
+                ],
+                'invalid_message' => 'fos_user.password.mismatch',
+                'constraints' => new Password(),
+                'help' => 'form.password_help',
+                'translation_domain' => 'FOSUserBundle'
+            ])
+//            ->add('roles', ChoiceType::class, [
+//                'label' => 'form.roles',
+//                'choices' => [
+//                    'form.option.nurse' => 'ROLE_NURSE'
+//                ],
+//                'multiple' => true,
+//                'expanded' => true,
+//                'required' => false,
+//                'translation_domain' => 'FOSUserBundle',
+//                'choice_translation_domain' => 'FOSUserBundle'
+//            ])
+//            ->add('agreeTerms', ToggleButtonType::class, [
+//                'mapped' => false,
+//                'required' => false,
+//                'label' => 'form.terms',
+//                'label_translation_parameters' => [ '%link%' => $this->router->generate('terms') ],
+//                'translation_domain' => 'FOSUserBundle',
+//                'constraints' => new IsTrue(['message' => 'terms.rejected'])
+//            ])
+        ;
+    }
+}
---
+++ src/Validator/Constraints/Password.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Nurschool project.
+ *
+ * (c) Nurschool <https://github.com/abbarrasa/nurschool>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nurschool\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ */
+class Password extends Constraint
+{
+    public $pattern = '/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/';
+    public $message = 'password.invalid';
+}
