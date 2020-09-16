97accec3977a91839718a76da78b859d9e0e5f52
Alberto Buitrago <abuitrago@estrategiasdeinversion.com>
2020-09-16 10:32:22 +0200

    Capture Doctrine events in EasyAdmin

---
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Nurschool\EventSubscriber;
+
+
+use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityPersistedEvent;
+use Nurschool\Event\RegisteredUserEvent;
+use Nurschool\Model\UserInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class EasyAdminSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            AfterEntityPersistedEvent::class => ['sendConfirmationEmail'],
+        ];
+    }
+
+    public function sendConfirmationEmail(BeforeEntityPersistedEvent $event, $eventName, EventDispatcherInterface $dispatcher)
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!($entity instanceof UserInterface)) {
+            return;
+        }
+
+        // Propagate the event to send the confirmation email
+        $dispatcher->dispatch(new RegisteredUserEvent($entity), RegisteredUserEvent::NAME);
+    }
+}
