1fe92b7d8dc8d201aa4bcbb472338a5f7353552e
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2021-02-11 23:15:26 +0100

    Reordering

---
+++ src/Forum/Model/DiscussionRepositoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Nurschool project.
+ *
+ * (c) Nurschool <https://github.com/abbarrasa/nurschool>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nurschool\Domain\Model\Modules\Forum\Repository;
+
+
+use Nurschool\Repository\RepositoryInterface;
+
+interface DiscussionRepositoryInterface extends RepositoryInterface
+{
+
+}
---
+++ src/Modules/User/Domain/Model/RepositoryInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Nurschool project.
+ *
+ * (c) Nurschool <https://github.com/abbarrasa/nurschool>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nurschool\Domain\Model;
+
+
+interface RepositoryInterface
+{
+    public function find($id, $lockMode = null, $lockVersion = null);
+
+    public function findOneBy(array $criteria, array $orderBy = null);
+
+    public function findAll();
+
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null);
+
+    public function save($object, bool $andFlush = true);
+
+    public function remove($object, bool $andFlush = true);
+}
